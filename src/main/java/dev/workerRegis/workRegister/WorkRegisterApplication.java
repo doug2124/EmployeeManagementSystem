fb2e2d28e68f14e8307e36c000746b6473c25d7d
c250368 <c250368@ccg.ac.jp>
2026-02-13 06:34:57 +0900

    first commit

---
+++ src/main/java/dev/workerRegis/workRegister/WorkRegisterApplication.java
@@ -0,0 +1,13 @@
+package dev.workerRegis.workRegister;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkRegisterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkRegisterApplication.class, args);
+	}
+
+}
